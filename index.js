87f877e801a69623d124f6ed62a858d5f5e40325
Promotie2 <163719152+Promotie2@users.noreply.github.com>
2024-03-29 11:25:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'onzichtbaar',
+    status: 'Invisible',
   });
 
   
